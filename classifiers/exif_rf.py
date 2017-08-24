6b3d25084aab831e6b1614e1d2ea46462e6885dc
WK <kyou.wk@gmail.com>
2017-08-24 17:29:43 +0800

    basic cnn classifier

--- classifiers/exif_rf.py
+++ classifiers/exif_rf.py
@@ -9,4 +9,23 @@ from sklearn.externals import joblib
 from sklearn.model_selection import train_test_split
 
 class ExifRF:
-    pass
+    ss_pattern = re.compile("^\d/.*")
+
+    def eval_shutterspeed(self,shutterspeed):
+        if self.ss_pattern.match(shutterspeed):
+            return 1/int(shutterspeed[2:])
+        else:
+            return shutterspeed
+
+    def classify(self, classifier,data):
+        
+        data['ExposureTime'] = data.apply(lambda row: self.eval_shutterspeed(row['ExposureTime']),axis=1)
+        outcome = classifier.predict(data)
+        print("Class: ", outcome )
+        outcome2 = classifier.predict_log_proba(data)
+        print("Class log prob: ",outcome2)
+        outcome3 = classifier.predict_proba(data)
+        print("Class prob: ",outcome3)
+
+    def load_c(fname):
+        return joblib.load(fname)
